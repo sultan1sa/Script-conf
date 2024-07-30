1baa1eb2e3327c2bc267ca427f3f945a554c56a1
sultan1sa <68920908+sultan1sa@users.noreply.github.com>
2024-07-30 11:11:54 +0300

    Update revenuecat.js

--- shadowrocket/shadiw/revenuecat.js
+++ shadowrocket/shadiw/revenuecat.js
@@ -1,105 +1,183 @@
-/*
-
-Aphrodite、Pandora、Apollo、VSCO、CountDuck、Happy Days、Awesome Habits、Anybox、Grow、Planny、我的物品、mizframa、事线、FaceGlow
+# ========解锁列表======== #
+https://appraven.net/collection/77299969
 
 [rewrite_local]
 
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/[^/]+$) url script-echo-response https://raw.githubusercontent.com/sultan1sa/Script-conf/main/shadowrocket/shadiw/revenuecat.js
-^https:\/\/api\.revenuecat\.com\/.+\/subscribers\/[^/]+/(offerings|attributes)$ url request-header (\r\n)X-RevenueCat-ETag:.+(\r\n) request-header $1X-RevenueCat-ETag:$2
+# ～ RevenueCat@ddgksf2013
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/[^/]+$) url script-response-body https://raw.githubusercontent.com/sultan1sa/Revenuecat/main/revenuecat.js 
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts|subscribers) url script-request-header https://raw.githubusercontent.com/sultan1sa/Revenuecat/main/deleteHeader.js
 
-[MITM]
-hostname = api.revenuecat.com
+[mitm]
 
-*/
+hostname=api.revenuecat.com
+
+***********************************/
+
+
+
 
 // ========= 动态ID ========= //
-const entitlementMapping = {
-  //'User-Agent':['权益名称','订阅标识符']
-  'Aphrodite': ['unlock','com.overdesigned.incognito.lifetime'],
-  'OneBox':['all', 'com.ziheng.OneBox'],
-    'askai': ['pro', 'aa_0199_1w'],
-  'madeyu': ['unlimited', 'my_999_1m'],
+const mapping = {
+  '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
+  'FinancialNote': ['category'],
+  'QingLong': ['Premium'],
+  'CircleTime/': ['Premium'],
+  'ScreenRecordCase/': ['Premium'],
+  'Chronicling/': ['Premium'],
+  'Yosum/': ['Premium'],
+  'Currency-Converter/': ['pro'],
+  'Precious/': ['Pro'],
+  'Unfold/': ['PRO'],
+  'mark_cup/': ['premiun'],
+  'SleepMaster/': ['premium','sm_14999_lifetime'],
+  'Grain/': ['gold','lifetimeMembership'],
+  'AudioPlayer': ['Pro'],
+  'FoJiCam/': ['ProVersionLifeTime'],
+  'pdfai_app/': ['premium'],
+  'LUTCamera': ['ProVersion', 'com.uzero.funforcam.monthlysub'],
+  'totowallet': ['all', 'com.ziheng.totowallet.yearly'],
+  'Aphrodite': ['all'],
+  'timetrack.io': ['atimelogger-premium-plus'],
+  'LiveWallpaper': ['Pro access'],
+  'SharkSMS': ['VIP','com.lixkit.diary.permanent_68'],
+  '%E7%BE%8E%E5%A6%86%E6%97%A5%E5%8E%86': ['Pro access'],
+  'Aula/': ['Pro access'],
+  'apollo': ['all'],
+  'LockFlow/': ['unlimited_access'],
+  'iplayTV/': ['com.ll.btplayer.12'],
+  'widget_art': ['all'],
+  'OneBox': ['all'],
+  'Taskbit/': ['Pro'],
+  'Spark': ['premium'],
+  'Pillow': ['premium'],
+  'DecibelMeter/': ['Premium'],
+  '1Blocker': ['premium'],
+  'VSCO': ['membership'],
+  'UTC': ['Entitlement.Pro'],
+  '%E8%AC%8E%E5%BA%95%E9%BB%91%E8%86%A0': ['Entitlement.Pro'],
+  '%E8%AC%8E%E5%BA%95%E6%99%82%E9%90%98': ['Entitlement.Pro'],
+  'OffScreen': ['Entitlement.Pro'],
+  'ScannerPro': ['plus'],
+  'Duplete/': ['Pro'],
+  'Ooga/': ['Ooga'],
+  'WhiteCloud': ['allaccess'],
+  'HTTPBot': ['pro'],
+  'audiomack': ['Premium1'],
+  'server_bee': ['Pro'],
+  'simple-': ['patron'],
+  'streaks': ['patron'],
+  'andyworks-calculator': ['patron'],
+  'vibes': ['patron'],
+  'CountDuck': ['premium', 'Lifetime'],
+  'IPTVUltra': ['premium'],
+  'Happy%3ADays': ['pro', 'happy_999_lifetime'],
+  'PDF_convertor/': ['VIP', 'com.pdf.convertor.forever'],
+  'ChatGPTApp': ['Advanced'],
+  'APTV': ['pro'],
+  'TouchRetouchBasic': ['premium'],
+  'My%20Jump%20Lab': ['lifetime'],
+  '%E7%9B%AE%E6%A0%87%E5%9C%B0%E5%9B%BE': ['pro'],
+  'Paku': ['pro'],
+  'Awesome%20Habits': ['premium'],
+  'askai': ['pro', 'aa_3999_1y_3d0'],
+  'MoneyThings': ['Premium'],
+  'Anybox': ['pro'],
+  'Fileball': ['filebox_pro'],
+  'Noto': ['pro'],
+  'Grow': ['grow.pro', 'grow_lifetime'],
+  'Grow': ['grow.pro', 'grow_lifetime'],
   'Grow': ['grow.pro', 'grow_lifetime'],
   'Grow': ['grow.pro', 'grow_lifetime'],
   'Grow': ['grow.pro', 'grow_lifetime'],
   'Locket': ['Gold'],
-  'apollo': ['all'],
-  'VSCO': ['membership'],
-  'CountDuck': ['premium', 'Lifetime'],
-  'Happy%3ADays': ['pro', 'happy_999_lifetime'],
-  'Awesome%20Habits': ['premium'],
-  'com.overdesigned.incognito.lifetime': ['unlock'],
-  'Grow': ['grow.pro', 'grow_lifetime'],
-  'Planny': ['premium'],
+  'My%20Tim': ['Pro'],
+  'Photom': ['premium', 'pixelmator_photo_pro_subscription_v1_pro_offer'],
   'mizframa': ['premium', 'mf_20_lifetime2'],
-  'Overdue': ['Pro'],//我的物品
-  '%E4%BA%8B%E7%BA%BF': ['bot_builder','bot_builder_subscription_annual'],//事线
-  'GetFace': ['Pro access'],//FaceGlow
+  'YzyFit/': ['pro', 'yzyfit_lft_v2'],
+  'ImageX': ['imagex.pro.ios', 'imagex.pro.ios.lifetime'],
+  'Fin': ['premium', 'com.circles.fin.premium.yearly'],
+  'Ledger': ['Pro', 'com.lifetime.pro'],
+  'One4Wall': ['lifetime', 'lifetime_key'],
+  'PhotoMark/': ['Pro', 'com.photo.mark.forever'],
+  'SimpleScan/': ['premium', 'com.atlantia.SimpleScan.Purchases.Lifetime'],
+  'OneWidget': ['allaccess'],
+  'CardPhoto': ['premium'],
+  'Journal_iOS/': ['PRO'],
+  'LemonKeepAccounts/': ['VIP','lm_1_1month'],
+  'PDF%20Viewer': ['sub.pro'],
+  'PhotoRoom': ['business'],
+  'Decision': ['com.nixwang.decision.entitlements.pro'],
+  'Tangerine': ['Premium'],
+  'PastePal': ['premium'],
+  'Fiery': ['premium'],
+  'Airmail': ['Airmail Premium'],
+  'Stress': ['StressWatch Pro'],
+  'PinPaper': ['allaccess'],
+  'Echo': ['PLUS'],
+  'MyThings': ['pro','xyz.jiaolong.MyThings.pro.infinity'],
+  'Overdue': ['Pro'],
+  'BlackBox': ['plus','app.filmnoir.appstore.purchases.lifetime'],
+  'Spektr': ['premium'],
+  'MusicMate': ['premium','mm_lifetime_68_premium'],
+  '%E4%BA%8B%E7%BA%BF': ['pro','xyz.jiaolong.eventline.pro.lifetime'],
+  'Tasks': ['Pro'],
+  'Currency': ['plus'],
+  'money_manager': ['premium'],
+  'fastdiet': ['premium'],
+  'Blurer': ['paid_access'],
+  'Everlog': ['premium'],
+  'reader': ['vip2','com.valo.reader.vip2.year'],
+  'GetFace': ['Pro access'],
+  'intervalFlow': ['All Access','wodtimer_lf_free'],
+  'Period%20Calendar': ['Premium','com.lbrc.PeriodCalendar.premium.yearly'],
+  'Cookie': ['allaccess','app.ft.Bookkeeping.lifetime'],
+  'ScientificCalculator': ['premium','com.simpleinnovation.calculator.ai.premium.yearly.base'],
+  'MOZE': ['premium'],
+  '1LemonKeepAccounts/': ['vip'],
+  'To%20Me/': ['Premium'],
+  '%E8%A8%80%E5%A4%96%E7%AD%86%E8%A8%98/': ['Premium'],
+  'alcohol.tracker': ['pro','drinklog_lifetime'],
+  'DayPoem': ['Pro Lifetime'],
+  'Budget%20Flow': ['full_access','com.fabian.hasse.haushaltsbuch.upgrade.combined'],
+  'G%20E%20I%20S%20T': ['memorado_premium'],
+  'multitimer_app': ['premium','timus_lt'],
+  'Darkroom': ['co.bergen.Darkroom.entitlement.allToolsAndFilters'],
+  'tiimo': ['full_access'],
+  'FaceMa/': ['Pro access'],
+  'Record2Text/': ['Pro access'],
+  'jinduoduo_calculator': ['jinduoduoapp','mobile_vip'],
+  'Focused%20Work': ['Pro'],
+  'GoToSleep': ['Pro'],
+  'kegel': ['kegel_pro'],
+  'Ochi': ['Pro'],
+  'Pomodoro': ['Plus','com.MINE.PomodoroTimer.plus.yearly'],
+  'universal/': ['Premium','remotetv.yearly.07'],
+  'ShellBean/': ['pro','com.ningle.shellbean.subscription.year'],
+  'AI%20Art%20Generator/': ['Unlimited Access'],
+  'Email%20Me': ['premium'],
+  'GoodThing/': ['pro','goodhappens_basic_year'],
+  'Reels%20Editor': ['Unlimited Access'],
+  'com.dison.diary': ['vip'],
+  'iRead': ['vip'],
+  'jizhi': ['jizhi_vip'],
+  'card/': ['vip'],
+  'EraseIt/': ['ProVersionLifeTime'],
+  'Alpenglow': ['newPro'],
+  'MindBreathYoga/': ['lifetimeusa'],
+  'MetadataEditor': ['unlimited_access'],
+  '%E6%9F%A5%E5%A6%86%E5%A6%86': ['Pro access'],
+  '%E5%85%83%E6%B0%94%E8%AE%A1%E6%97%B6': ['plus'],
+  'WidgetCat': ['MiaoWidgetPro'],
+  'Emphasis/': ['premium'],
+  'FormScanner/': ['Pro','formscanner_lifetime'],
+  'streamer/': ['Premium'],
+  'NeatNook/': ['com.neatnook.pro','com.neatnook.pro.forever'],
+  'Blackout/': ['premium','blackout_299_lt'],
+  'Budgetify/': ['premium','budgetify_3999_lt'],
+  'Dedupe/': ['Pro','com.curiouscreatorsco.Dedupe.pro.lifetime.notrial.39_99'],
+  'Wozi': ['wozi_pro_2023']
 };
 
-// ========= 固定部分 ========= // 
-// ========= @ddgksf2021 ========= // 
-var userAgent = $request.headers["User-Agent"] || $request.headers["user-agent"];
-
-var metadata = {
-  AlxXxlA: "原数据！",
-  request_date_ms: 1684383920000,//请求日期的毫秒数
-  request_date: "2023-05-18T12:25:20Z",//请求日期的字符串格式
-  subscriber: {
-    non_subscriptions: {},
-    first_seen: "2023-05-18T12:25:20Z",//首次见到的日期
-    original_application_version: "7",//原始应用程序版本
-    other_purchases: {},//其他购买的对象
-    management_url: "https://apps.apple.com/account/subscriptions",//管理订阅的网址
-    subscriptions: {},//订阅的对象
-    entitlements: {},//权益的对象
-    original_purchase_date: "2023-05-17T12:25:20Z",//原始购买日期
-    original_app_user_id: "$RCAnonymousID:AlxXxlA",//原始应用程序用户的标识符
-    last_seen: "2023-05-18T12:25:20Z"//最后见到的日期
-  }//订阅者的信息，作为元数据的初始模板，后面会根据用户的 User-Agent 来修改其中的 subscriptions 和 entitlements 字段。
-};
-
-var subscriptionInfo = {
-  is_sandbox: false,
-  ownership_type: "PURCHASED",
-  billing_issues_detected_at: null,
-  period_type: "normal",
-  expires_date: "2099-05-18T12:25:20Z",//过期日期
-  grace_period_expires_date: null,//宽限期过期日期
-  unsubscribe_detected_at: null,//发现取消订阅的日期
-  original_purchase_date: "2023-05-18T12:25:20Z",//原始购买日期
-  purchase_date: "2023-05-18T12:25:20Z",//购买日期
-  store: "app_store"
-};//作为订阅的对象的模板，后面会根据用户的 User-Agent 来修改其中的 product_identifier 字段。
-
-var premiumSubscription = {
-  grace_period_expires_date: null,
-  purchase_date: "2023-05-18T12:25:20Z",//购买日期
-  product_identifier: "alxxxla",//产品标识符
-  expires_date: "2099-05-18T12:25:20Z"//过期日期
-};//这个对象的作用是作为权益的对象的模板，后面会根据用户的 User-Agent 来修改其中的 product_identifier 字段。
-
-var metadataObject = JSON.parse(JSON.stringify(metadata));
-premiumSubscription.product_identifier = "com.alxxxla.premium.yearly";
-metadataObject.subscriber.subscriptions["com.alxxxla.premium.yearly"] = subscriptionInfo;
-//使用 Object.keys(entitlementMapping) 方法，获取 entitlementMapping 对象的所有键，返回一个包含所有键的数组，然后使用 find 方法，遍历这个数组，对每个键执行一个回调函数，该函数的参数是 key，表示当前遍历的键，函数的返回值是一个布尔值，表示是否找到了匹配的键。回调函数的逻辑是使用 userAgent.includes(key) 方法，判断用户的 User-Agent 是否包含当前遍历的键，如果包含，则返回 true，否则返回 false。find 方法的返回值是第一个使回调函数返回 true 的键，或者如果没有找到匹配的键，则返回 undefined。将 find 方法的返回值赋值给一个变量 matchedKey，这个变量的作用是表示用户的 User-Agent 匹配的应用程序的名称，或者如果没有匹配的应用程序，则为 undefined。
-const matchedKey = Object.keys(entitlementMapping).find(key => userAgent.includes(key));
-if (matchedKey) {//判断 matchedKey 是否存在
-  //使用 let 关键字，定义两个变量 entitlementKey 和 subscriptionKey，它们的值是从 entitlementMapping 对象中，根据 matchedKey 作为键，获取对应的值，该值是一个数组，包含两个元素，第一个元素赋值给 entitlementKey，第二个元素赋值给 subscriptionKey。这两个变量的作用是表示用户的 User-Agent 匹配的应用程序的权益名称和订阅标识符。
-  let [entitlementKey, subscriptionKey] = entitlementMapping[matchedKey];
-  if (subscriptionKey) {//判断 subscriptionKey 是否存在
-    /*
-    将 premiumSubscription 对象的 product_identifier 字段的值设置为 subscriptionKey，这表示用户订阅了匹配的应用程序的订阅标识符。
-    将 metadataObject 对象的 subscriber 对象的 subscriptions 对象的 subscriptionKey 字段的值设置为 subscriptionInfo 对象，这表示用户订阅了匹配的应用程序的订阅信息。
-    */
-    premiumSubscription.product_identifier = subscriptionKey;
-    metadataObject.subscriber.subscriptions[subscriptionKey] = subscriptionInfo;
-  }
-  //将 metadataObject 对象的 subscriber 对象的 entitlements 对象的 entitlementKey 字段的值设置为 premiumSubscription 对象，这表示用户拥有匹配的应用程序的权益信息。
-  metadataObject.subscriber.entitlements[entitlementKey] = premiumSubscription;
-} else {//如果 matchedKey 不存在
-  //将 metadataObject 对象的 subscriber 对象的 entitlements 对象的 pro 字段的值设置为 premiumSubscription 对象，这表示用户拥有默认的 pro 权益信息。
-  metadataObject.subscriber.entitlements.pro = premiumSubscription;
-}
-//将 metadataObject 对象转换为 JSON 格式的字符串。将元数据对象作为响应的正文返回给用户。
-$done({ body: JSON.stringify(metadataObject) });
+// =========    固定部分  ========= // 
+// =========  @ddgksf2021 ========= // 
+var ua=$request.headers["User-Agent"]||$request.headers["user-agent"],obj=JSON.parse($response.body);obj.Attention="恭喜你抓到元数据！由墨鱼分享，请勿售卖或分享他人！";var ddgksf2013={is_sandbox:!1,ownership_type:"PURCHASED",billing_issues_detected_at:null,period_type:"normal",expires_date:"2099-12-18T01:04:17Z",grace_period_expires_date:null,unsubscribe_detected_at:null,original_purchase_date:"2022-09-08T01:04:18Z",purchase_date:"2022-09-08T01:04:17Z",store:"app_store"},ddgksf2021={grace_period_expires_date:null,purchase_date:"2022-09-08T01:04:17Z",product_identifier:"com.ddgksf2013.premium.yearly",expires_date:"2099-12-18T01:04:17Z"};const match=Object.keys(mapping).find(e=>ua.includes(e));if(match){let[e,s]=mapping[match];s?(ddgksf2021.product_identifier=s,obj.subscriber.subscriptions[s]=ddgksf2013):obj.subscriber.subscriptions["com.ddgksf2013.premium.yearly"]=ddgksf2013,obj.subscriber.entitlements[e]=ddgksf2021}else obj.subscriber.subscriptions["com.ddgksf2013.premium.yearly"]=ddgksf2013,obj.subscriber.entitlements.pro=ddgksf2021;$done({body:JSON.stringify(obj)});
